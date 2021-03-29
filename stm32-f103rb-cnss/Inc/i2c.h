8eb53969246d155d8cfd5bcc6cad8e10f812d8d0
NaomiCreate <naomiresnik@gmail.com>
2021-03-30 00:19:16 +0300

    i2c.c

--- stm32-f103rb-cnss/Inc/i2c.h
+++ stm32-f103rb-cnss/Inc/i2c.h
@@ -8,6 +8,8 @@
 #ifndef I2C_H_
 #define I2C_H_
 
+void init_i2c(void);
 
+void i2c_write_single(uint8_t device_address,uint8_t mem_address, uint8_t data);
 
 #endif /* I2C_H_ */
--- stm32-f103rb-cnss/Src/i2c.c
+++ stm32-f103rb-cnss/Src/i2c.c
@@ -19,20 +19,72 @@
 */
 
 /*---------Init I2c---------*/
-//RCC->APB2ENR |= RCC_APB2ENR_IOBEN //iO port B clock enable(P. 114)
+void init_i2c(void){
+	//RCC->APB2ENR |= RCC_APB2ENR_IOBEN //iO port B clock enable(P. 114)
 
-//RCC->APB1ENR &= RCC_APB1ENR_I2C1EN //enable I2C_1(P. 115)
-//RCC->APB1ENR &= RCC_APB1ENR_I2C2EN //enable I2C_2(P. 115)
+	//RCC->APB1ENR &= RCC_APB1ENR_I2C1EN //enable I2C_1(P. 115)//?
+	//RCC->APB1ENR &= RCC_APB1ENR_I2C2EN //enable I2C_2(P. 115)//?
 
-//I2C2->CR2 |= 36; //freq value of APB1 buss=72/2=36 (p.774)
-/*---------send a start condition---------*/
-//I2C_CR1 START=1 //generate our start condition(p. 772)
+	//??RCC- enable AFIO??
 
-/*---------Wait---------*/
-//until I2C_CR2 SB=1 //tells us that the start condition successfully sent(p.777)
+	//I2C2->CR2 |= 36; //freq value of APB1 buss=72/2=36 (p.774)
+	//I2C2->CCR |= 180; //speed to 100kHz (standart mode max)
+	//I2C2->TRISE |= 37; //1000 ns/ (CR2 period =1/36MHz) = TRISE +1
 
-/*---------send the slave's address---------*/
-//write it in I2C_OAR1 (p. 776)
+	//I2C2->CR1 |= I2C_cr1_ACK; //enable acks
 
-/*---------Wait---------*/
-//until I2C_CR2 ADDR=1//tells us that there was an address match(p. 777)
+	//stretch mode enabled by default
+
+	//7 bit addressing mode enabled by default
+
+	//config gpio for i2c use enable internal pull up
+	//GPIOB->CRH |= GPIO_CRH_MODE10 | GPIO_CRH_CNF11 | GPIO_CRH_MODE11; //alt function output open drain(p. 168, Table 27. I2C +p.172)
+	//GPIOB->ODR |= (1<<10) | (1<<11);
+
+
+	//I2C2->CR1 |= I2C_CR1_PE; //enable peripheral needs to be done last
+}
+void i2c_write_single(uint8_t device_address,uint8_t mem_address, uint8_t data){
+
+	//uint32_t temp; //will help us clear ADDR bit
+
+	/*---------send a start condition---------*/
+	//I2C->CR1 |= I2C_CR1_START //generate our start condition(p. 772)
+
+	/*---------Wait until we know that the start condition arrived---------*/
+	//while(!(I2C2->SR1 & I2C_SR1_SB))//until I2C_CR2 SB=1 //tells us that the start condition successfully sent(p.777)
+	//{
+	//}
+
+	/*---------send the slave's address by writing it in the data register---------*/
+	//I2C->DR = device_address;//write it in I2C_OAR1 (p. 776)
+
+	/*---------Wait until we know that there was an address match---------*/
+	//while(!(I2C2->SR1 & I2C_SR1_ADDR))//until I2C_CR2 ADDR=1//tells us that there was an address match(p. 777)
+	//{
+	//}
+
+	//We will read SR2 bit( ADDR bit is cleared by software reading SR1 register followed reading SR2(p. 780))
+	//temp = I2C2->SR2;
+
+	/*---------enter the memory/internal address to write to---------*/
+	//I2C2->DR = mem_address;//address to write to
+
+	/*---------wait for the address transfer to complete---------*/
+	//while(!(I2C2->SR1 & I2C_SR1_TXE))
+	//{
+	//}
+
+	/*---------write data to the address---------*/
+	//I2C2->DR = data;//data to write in that address
+
+	/*---------wait for the data transfer to complete---------*/
+	//while(!(I2C2->SR1 & I2C_SR1_TXE))
+	//{
+	//}
+
+	/*---------send another start condition to get the data---------*/
+	//I2C2->CR1 |=I2C_CR1_STOP;
+}
+
+
