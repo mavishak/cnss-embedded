4ecd405ec3c01e8c28a19455612cc77e0f8c0b9b
mavishak <mayanavital@gmail.com>
2021-04-13 14:51:19 +0300

    uint8_t I2C1_Read1Byte(uint8_t DeviceAddr, uint8_t addr)
    need to double check and test on hardware

--- stm32-f103rb-cnss/Inc/i2c.h
+++ stm32-f103rb-cnss/Inc/i2c.h
@@ -8,8 +8,14 @@
 #ifndef I2C_H_
 #define I2C_H_
 
+#include "stm32f103xb.h" //this file is included to every header file
+#include "common.h"
+
 
 void init_i2c1(void);
+uint8_t I2C1_Read1Byte(uint8_t DeviceAddr, uint8_t addr);
+
+
 void I2C_start(uint8_t address);//, uint8_t direction);
 void I2C_write(uint8_t data);
 void I2C_stop();
--- stm32-f103rb-cnss/Src/i2c.c
+++ stm32-f103rb-cnss/Src/i2c.c
@@ -20,7 +20,7 @@
 #include <stdio.h>
 
 static uint32_t temp; //will help us clear ADDR bit
-static uint32_t i2c_buff; //the address of the buffer= where we want to store the data we receive(short explanation on the reference manual p. 277)
+static uint8_t byte1_buff; //the address of the buffer= where we want to store the data we receive(short explanation on the reference manual p. 277)
 
 /*starting our communication in I2C:
 *1.	send a start condition
@@ -32,29 +32,33 @@ static uint32_t i2c_buff; //the address of the buffer= where we want to store th
 
 void init_i2c1(void){
 
+	/*Enable use of SCL (PB6) and SDA (PB7)*/
+	RCC->APB2ENR |= 0x00000008;  // [referance manual 7.3.7]
 
 	/*Enable I2C1 RCC clock*/
-	RCC->APB1ENR |= 0x00200000;
+	RCC->APB1ENR |= 0x00200000; // [referance manual 7.3.8]
 
-	/*Enable use of SCL (PB6) and SDA (PB7)*/
-	RCC->APB2ENR |= 0x00000008;
+
+	//Configure PB6 and PB7 as SCL and SDA (respectively)
 
 	/*Enable AFIO*/
 	RCC->APB2ENR |= 0x00000001; // ... test with and without
 
 	/*Set SCL (PB6) and SDA (PB7) [reference manual p. 168 Table  27]*/
-	GPIOB->CRL &= 0x00FFFFFF; // reset relevebt bits (see: reference manual 9.2.1)
+	GPIOB->CRL &= 0x00FFFFFF; // reset relevent bits [reference manual 9.2.1]
 	GPIOB->CRL |= 0xEE000000; // set as Alternate function output Open-drain, max speed 2 MHz.
 
 
 
-	/*Set I2C Configurations*/
+	//Set I2C Configurations
 
 	/*Enable ACK*/
-	I2C1->CR1 |= 0x0400;
+	//I2C1->CR1 |= 0x0400; //enable after wards if needed
 
 
 	//The following is the required sequence in master mode. [reference manual p. 758]
+
+	//I2C_OAR1[ADD0]: 7-bit addressing mode: don’t care [referense manual 26.6.3]
 
 	/*Set Peripheral clock frequency*/
 	I2C1->CR2 &= 0xFFC0; // reset relavent bits FREQ[5:0]
@@ -63,21 +67,18 @@ void init_i2c1(void){
 	/*Set CCR register: http://fastbitlab.com/stm32-i2c-lecture-12-i2c-serial-clock-settings-with-explanations/
 	 *See: [reference manual 26.6.8], [datasheet p. 59 Table 40]*/
 
-	/*Set CCR to generate 100kHz frequency in Sm mode (referance manual p. 782 "For intance"*/
+	/*Set CCR to generate 100kHz frequency in Sm mode [referance manual p. 782 "For intance"]*/
 	I2C1->CCR = 0x0028;
 
 
 	/* Configure the rise time register: TRISE*/
 
-	/*Set TRISE to maximum allowed SCL rise time in Sm mode (referance manual p. 783 "For intance".*/
+	/*Set TRISE to maximum allowed SCL rise time in Sm mode [referance manual p. 783 "For intance"].*/
 	I2C1->TRISE = 0x0009;
 
 
 	/*Enable I2C peripheral (PE = 1)*/
 	I2C1->CR1 |= 0x0001; //The CCR register must be configured only when the I2C is disabled (PE = 0) [reference manual p. 781 26.6.8].
-
-
-
 
 
 
@@ -122,6 +123,87 @@ void init_i2c1(void){
 }
 
 
+/* OV7670 DeviceAddr:
+ * 0x42 write (Transmitter mode)
+ * 0x43 Read (Receiver mode) = 0x42 | 0x01
+ * DeviceAddr can be any one of them*/
+uint8_t I2C1_Read1Byte(uint8_t DeviceAddr, uint8_t addr){
+
+
+	//Transmitter mode
+
+	/*wait if I2C1 is busy [reference manual p. 758 | section 26.3.3]*/
+	while((I2C1->SR2 & 0x0002) == 0x0002); // BUSY bit [reference manual 26.6.7]
+
+	/*generate START [reference manual p. 760 | Figure 273. [S]]*/
+	I2C1->CR1 |= 0x0100; // START bit [reference manual 26.6.1]
+
+	/*wait EV5: wait until start condition arrived [refernce manual p. 760 | Figure 273. [EV5]]*/
+	while((I2C1->SR1 & 0x0001) == 0x0001); // SB bit [reference manual 26.6.6]
+
+	//SB=1: Cleared by software by reading the SR1 register followed by writing the DR register
+
+	/*send device addr [refernce manual p. 760 | Figure 273. [EV5]]*/
+	I2C1->DR = (uint8_t)((DeviceAddr & ~(0x01))); // [reference manual p. 759 'To enter Transmitter mode']
+
+	/*wait EV6: wait until address is recieved and matched [refernce manual p. 760 | Figure 273. [EV6]]*/
+	while((I2C1->SR1 & 0x0002) != 0x0002); // ADDR bit [refernce manual 26.6.6]
+
+	//ADDER=1: Cleared by software reading SR1 register followed reading SR2
+
+	/*addr within device*/
+	temp = I2C1->SR2;
+
+	/*wait for EV8_2 [reference manual p. 760 | Figure 273. [EV8_2]]*/
+	while((I2C1->SR1 & 0x0080) != 0x0080); // TxE bit [reference manual 26.6.6]
+
+	// ???
+	//generate STOP
+	//delay 1000 ms
+	//wait if I2C1 is busy
+	// ???
+
+	////Receiver mode
+
+	/*generate START [reference manual p. 761 | Figure 274. [S]]*/
+	I2C1->CR1 |= 0x0100; // START bit [reference manual 26.6.1]
+
+	/*wait EV5: wait until start condition arrived [refernce manual p. 761 | Figure 274. [EV5]]*/
+	while((I2C1->SR1 & 0x0001) == 0x0001); // SB bit [reference manual 26.6.6]
+
+	/*send device addr [refernce manual p. 760 | Figure 273. [EV5]]*/
+	I2C1->DR = (uint8_t)((DeviceAddr | 0x01)); // [reference manual p. 759 'To enter Receiver mode']
+
+	/*wait EV6: wait until address is recieved and matched [refernce manual p. 761 | Figure 274. [EV6]]*/
+	while((I2C1->SR1 & 0x0002) != 0x0002); // ADDR bit [refernce manual 26.6.6]
+
+	/*do not acknowledge [refernce manual p. 763 | 'Case of a single byte to ber received']*/
+	I2C1->CR1 &= ~(0x0400); // ACK bit [reference manual 26.6.1]
+
+	/*wait EV7 [refernce manual p. 761 | Figure 274. [EV7]]*/
+	while((I2C1->SR1 & 0x0040) != 0x0040); // RxE bit [reference manual 26.6.6]
+
+	/*retrieve byte [reference manual p. 763 | 'Case of a single byte to ber received']*/
+	byte1_buff = I2C1->DR;
+
+	/*Generate stop condition [reference manual p. 763 | 'Case of a single byte to ber received']*/
+	I2C2->CR1 |= 0x0200; // STOP bit [reference manual 26.6.1]
+
+	return byte1_buff;
+
+}
+
+
+
+
+//!----ALL THE REST IS DRAFTS --------!//
+
+
+
+
+
+
+
 /*Device address 0x42 for write, 0x43 for read [OV7670 datasheet p. 12]*/
 void I2C_start(uint8_t address){//, uint8_t direction){
 
@@ -138,10 +220,10 @@ void I2C_start(uint8_t address){//, uint8_t direction){
 	//SB=1: Cleared by software by reading the SR1 register followed by writing the DR register
 
 	/*Write address*/
-	I2C1->DR = (uint8_t)(device_address & 0xFF);
+	I2C1->DR = (uint8_t)(address & 0xFF);
 
 	/*Wait until address is recieved and matched [refernce manual 26.6.6]*/
-	while(!(I2C1->SR1 & 0x0002) == 0x0002); // ADDR bit
+	while((I2C1->SR1 & 0x0002) != 0x0002); // ADDR bit
 
 	//ADDER=1: Cleared by software reading SR1 register followed reading SR2
 
@@ -156,7 +238,7 @@ void I2C_write(uint8_t data){
 	I2C2->DR = (uint8_t)(data & 0xFF);
 
 	/*Wait data to be received*/
-	while((I2C2->SR1 & 0x0080) == 0x0080); //TXE bit
+	while((I2C1->SR1 & 0x0080) == 0x0080); //TXE bit
 
 	//Cleared by software writing to the DR register (referance manual 26.26.6)
 }
@@ -168,8 +250,18 @@ void I2C_stop(){
 	I2C2->CR1 |= 0x0200;//(reference manual 26.6.1)
 }
 
+/*Case of single byte to be received*/
 uint8_t I2C_read_nack(){
-	//?????
+
+	/*[reference manual p. 763]*/
+
+	/*In ADDR event clear ACK bit*/
+	I2C1->CR1 &= ~(0x0400);
+
+	return 0; //for now
+
+
+
 }
 
 
@@ -191,106 +283,106 @@ void test_communication(void){
 
 
 //using regular flags (not DMA)
-void i2c_write(uint8_t device_address,uint8_t mem_address, uint8_t data, uint8_t length){
-
-
-	/*---------Maybe according to Reference manual p. 760, Figure 273. Transfer sequence diagram for master transmitter(7-bit master transmitter-maybe we should 10-bit???)---------*/
-
-	/*send a start condition: S (see: Ref Manual 760)*/
-	I2C2->CR1 |= I2C_CR1_START; //generate our start condition(Reference manual p. 772)
-
-	/*Wait until we know that the start condition arrived: EV5 (see: Ref Manual 760)*/
-	while((I2C2->SR1 & 0x0001) == 0x0001);// wait until I2C_CR2 SB=1, start condition is sent successfully (Ref Manual p.777)
-
-
-	/*send the slave's address by writing it in the data register: Address (see: Ref Manual 760)*/
-	I2C2->DR = (uint8_t)(device_address & 0xFF);// (Reference manual p. 777) //maybe we need to write here:0x42 because of:(OV7670/OV7171 Advanced Information Preliminary Datasheet p. 11)
-
-	/*Wait until we know that there was an address match: EV6 (see: Ref Manual 760)*/
-	while(!(I2C2->SR1 & 0x0002) == 0x0002);//wait until I2C_CR2 ADDR=1, there was an address match( Ref Manual p. 777)
-
-	//Reading by software SR1 register followed by reading SR2  clears ADDR bit(p. 780))
-	temp = I2C2->SR2;//clears ADDR
-
-
-	/*Enter the internal memory address of the slave to write to: EV8_1 - Data1 ??? (see: Ref Manual 760)*/
-	I2C2->DR = (uint8_t)(mem_address & 0xFF);//address to write to//maybe the Address(Hex) in the table 5 in OV7670/OV7171 Advanced Information Preliminary Datasheet p. 11
-
-	/*Wait for the address transfer to complete - cleared by writing DR register*/
-	while((I2C2->SR1 & 0x0080) == 0x0080); // wait until TXE = 1
-
-
-	//now we can put data in the register
-	while(length > 0)
-	{
-		/*write data to the address*/
-		I2C2->DR = (uint8_t)(data & 0xFF);//data to write in that address
-
-		/*wait for the data transfer to complete*/
-		while((I2C2->SR1 & 0x0080) == 0x0080); // wait until TXE = 1
-
-		length--;
-	}
-
-	/*send stop condition: EV8_2 (see: Ref Manual 760)*/
-	I2C2->CR1 |=I2C_CR1_STOP;//(Reference manual p. 772)
-}
-
-
-
-//Using DMA
-void i2c_read(uint8_t device_address, uint8_t length){
+//void i2c_write(uint8_t device_address,uint8_t mem_address, uint8_t data, uint8_t length){
+//
+//
+//	/*---------Maybe according to Reference manual p. 760, Figure 273. Transfer sequence diagram for master transmitter(7-bit master transmitter-maybe we should 10-bit???)---------*/
+//
+//	/*send a start condition: S (see: Ref Manual 760)*/
+//	I2C2->CR1 |= I2C_CR1_START; //generate our start condition(Reference manual p. 772)
+//
+//	/*Wait until we know that the start condition arrived: EV5 (see: Ref Manual 760)*/
+//	while((I2C2->SR1 & 0x0001) == 0x0001);// wait until I2C_CR2 SB=1, start condition is sent successfully (Ref Manual p.777)
+//
+//
+//	/*send the slave's address by writing it in the data register: Address (see: Ref Manual 760)*/
+//	I2C2->DR = (uint8_t)(device_address & 0xFF);// (Reference manual p. 777) //maybe we need to write here:0x42 because of:(OV7670/OV7171 Advanced Information Preliminary Datasheet p. 11)
+//
+//	/*Wait until we know that there was an address match: EV6 (see: Ref Manual 760)*/
+//	while(!(I2C2->SR1 & 0x0002) == 0x0002);//wait until I2C_CR2 ADDR=1, there was an address match( Ref Manual p. 777)
+//
+//	//Reading by software SR1 register followed by reading SR2  clears ADDR bit(p. 780))
+//	temp = I2C2->SR2;//clears ADDR
+//
+//
+//	/*Enter the internal memory address of the slave to write to: EV8_1 - Data1 ??? (see: Ref Manual 760)*/
+//	I2C2->DR = (uint8_t)(mem_address & 0xFF);//address to write to//maybe the Address(Hex) in the table 5 in OV7670/OV7171 Advanced Information Preliminary Datasheet p. 11
+//
+//	/*Wait for the address transfer to complete - cleared by writing DR register*/
+//	while((I2C2->SR1 & 0x0080) == 0x0080); // wait until TXE = 1
+//
+//
+//	//now we can put data in the register
+//	while(length > 0)
+//	{
+//		/*write data to the address*/
+//		I2C2->DR = (uint8_t)(data & 0xFF);//data to write in that address
+//
+//		/*wait for the data transfer to complete*/
+//		while((I2C2->SR1 & 0x0080) == 0x0080); // wait until TXE = 1
+//
+//		length--;
+//	}
+//
+//	/*send stop condition: EV8_2 (see: Ref Manual 760)*/
+//	I2C2->CR1 |=I2C_CR1_STOP;//(Reference manual p. 772)
+//}
+//
+//
+//
+////Using DMA
+//void i2c_read(uint8_t device_address, uint8_t length){
 	//uint32_t temp = 0; //will help to clear flags
 
-	/*---------enable the DMA---------*/
-	RCC->AHBENR |= RCC_AHBENR_DMA1EN;// DMA1 clock enable (Reference manual p. 111)
-	I2C2->CR2 |= I2C_CR2_DMAEN;// DMA requests enable (Reference manual p. 774)
-
-	//????????????Maybe the next line redundant because we wrote it in init_i2c()???????????:
-	I2C2->CR1 |= I2C_CR1_ACK; //enable acknowledges
-
-	/*---------configure the DMA Channel procedure(Reference manual p.278)---------*/
-	/*Why channel 5? because of Reference manual p.281+282*/
-	//The first transfer address is the one programmed in the DMA_CPARx/DMA_CMARx registers(Reference manual p. 277)
-
-	//1.Setting the peripheral register address in the DMA_CPARx register.
-	DMA1_Channel5->CPAR = (uint32_t)&I2C2->DR;//the address of the data register of the i2c(the address of where we reading from)(short explanation on the reference manual p. 277)
-
-	//2.Setting the memory address in the DMA_CMARx register.
-	DMA1_Channel5->CMAR = (uint32_t)i2c_buff;//the address of the buffer= where we want to store the data we receive(short explanation on the reference manual p. 277)
-
-	//3.Configure the total number of data to be transferred in the DMA_CNDTRx register (After each peripheral event, this value will be decremented.)
-	DMA1_Channel5->CNDTR = length; //the number of bytes we receiving(the number of transfers from the data register(I2C2->DR) to the buffer(i2c_buff)(short explanation on the reference manual p. 277)
-
-	//4.Configure the channel priority using the PL[1:0] bits in the DMA_CCRx register (Reference manual p.286)
-
-	//5.Configure data transfer direction, circular mode, peripheral & memory incremented mode, peripheral & memory data size, and interrupt after half and/or full transfer in the DMA_CCRx register
-	DMA1_Channel5->CCR |= DMA_CCR_TCIE; //Transfer complete interrupt enable for the DMA (Reference manual p.286)
-	DMA1_Channel5->CCR |= DMA_CCR_MINC; //Memory increment mode enabled (Reference manual p.286)
-
-	//6.Activate the channel by setting the ENABLE bit in the DMA_CCRx register.
-	DMA1_Channel5->CCR |= DMA_CCR_EN; // Channel enable (Reference manual p.286)
-
-	/*---------generate start condition---------*/
-	I2C2->CR1 |= I2C_CR1_START; //(Reference manual p.772)
-	while(!(I2C2->SR1 & I2C_SR1_SB)) //wait until the SB flag is set
-	{
-	}
-
-	/*---------send address---------*/
-	I2C2->DR = device_address +1;
-	while(!(I2C2->SR1 & I2C_SR1_ADDR))//wait for the device match
-	{
-	}
-
-	temp = I2C2->SR2;//causing to clear ADDR flag
-
-	/*---------wait for the transfer to complete from the DMA---------*/
-	while((DMA1->ISR & DMA_ISR_TCIF1) == 0);
-
-	/*---------sending stop condition---------*/
-	I2C2->CR1 |= I2C_CR1_STOP;
-}
+//	/*---------enable the DMA---------*/
+//	RCC->AHBENR |= RCC_AHBENR_DMA1EN;// DMA1 clock enable (Reference manual p. 111)
+//	I2C2->CR2 |= I2C_CR2_DMAEN;// DMA requests enable (Reference manual p. 774)
+//
+//	//????????????Maybe the next line redundant because we wrote it in init_i2c()???????????:
+//	I2C2->CR1 |= I2C_CR1_ACK; //enable acknowledges
+//
+//	/*---------configure the DMA Channel procedure(Reference manual p.278)---------*/
+//	/*Why channel 5? because of Reference manual p.281+282*/
+//	//The first transfer address is the one programmed in the DMA_CPARx/DMA_CMARx registers(Reference manual p. 277)
+//
+//	//1.Setting the peripheral register address in the DMA_CPARx register.
+//	DMA1_Channel5->CPAR = (uint32_t)&I2C2->DR;//the address of the data register of the i2c(the address of where we reading from)(short explanation on the reference manual p. 277)
+//
+//	//2.Setting the memory address in the DMA_CMARx register.
+//	DMA1_Channel5->CMAR = (uint32_t)i2c_buff;//the address of the buffer= where we want to store the data we receive(short explanation on the reference manual p. 277)
+//
+//	//3.Configure the total number of data to be transferred in the DMA_CNDTRx register (After each peripheral event, this value will be decremented.)
+//	DMA1_Channel5->CNDTR = length; //the number of bytes we receiving(the number of transfers from the data register(I2C2->DR) to the buffer(i2c_buff)(short explanation on the reference manual p. 277)
+//
+//	//4.Configure the channel priority using the PL[1:0] bits in the DMA_CCRx register (Reference manual p.286)
+//
+//	//5.Configure data transfer direction, circular mode, peripheral & memory incremented mode, peripheral & memory data size, and interrupt after half and/or full transfer in the DMA_CCRx register
+//	DMA1_Channel5->CCR |= DMA_CCR_TCIE; //Transfer complete interrupt enable for the DMA (Reference manual p.286)
+//	DMA1_Channel5->CCR |= DMA_CCR_MINC; //Memory increment mode enabled (Reference manual p.286)
+//
+//	//6.Activate the channel by setting the ENABLE bit in the DMA_CCRx register.
+//	DMA1_Channel5->CCR |= DMA_CCR_EN; // Channel enable (Reference manual p.286)
+//
+//	/*---------generate start condition---------*/
+//	I2C2->CR1 |= I2C_CR1_START; //(Reference manual p.772)
+//	while(!(I2C2->SR1 & I2C_SR1_SB)) //wait until the SB flag is set
+//	{
+//	}
+//
+//	/*---------send address---------*/
+//	I2C2->DR = device_address +1;
+//	while(!(I2C2->SR1 & I2C_SR1_ADDR))//wait for the device match
+//	{
+//	}
+//
+//	temp = I2C2->SR2;//causing to clear ADDR flag
+//
+//	/*---------wait for the transfer to complete from the DMA---------*/
+//	while((DMA1->ISR & DMA_ISR_TCIF1) == 0);
+//
+//	/*---------sending stop condition---------*/
+//	I2C2->CR1 |= I2C_CR1_STOP;
+//}
 
 
 
